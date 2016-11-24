36316f8e2ee7034f96efbdb28022ecc077a56576
Simon Moll <moll@cs.uni-saarland.de>
2016-11-24 19:35:47 +0100

    added uniform test case with scalar rv_any

---
+++ test/launcher/verify_unifoo2f.cpp
@@ -0,0 +1,36 @@
+/*
+ * foo_launcher.cpp
+ *
+ *  Created on: Jul 22, 2015
+ *      Author: Simon Moll
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+#include <cassert>
+
+#include "launcherTools.h"
+
+extern "C" float foo(float a, float b);
+extern "C" float foo_SIMD(float a, float b);
+
+int main(int argc, char ** argv) {
+  const uint numInputs = 100;
+
+  for (unsigned i = 0; i < numInputs; ++i) {
+    float a = (float) rand();
+    float b = (float) rand();
+    float expectedRes = foo(a, b);
+    float simdRes = foo_SIMD(a, b);
+
+    if (expectedRes != simdRes) {
+      std::cerr << "MISMATCH!\n";
+      std::cerr << i << " : a = " << a << " b = " << b << " expected result " << expectedRes << " but was " << simdRes << "\n";
+      return -1;
+    }
+  }
+
+  return 0;
+}
---
+++ test/suite/test_007_scalarany-wfv-unifoo2f_UUrU.cpp
@@ -0,0 +1,7 @@
+extern "C" bool rv_any(bool);
+
+extern "C" float
+foo(float a, float b)
+{
+  return rv_any(a > b) ? a : b;
+}
