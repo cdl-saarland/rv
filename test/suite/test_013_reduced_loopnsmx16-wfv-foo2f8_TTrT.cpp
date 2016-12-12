dba6a22ba9863f881264931e59602a4d046d17a3
Simon Moll <moll@cs.uni-saarland.de>
2016-12-12 18:28:02 +0100

    test_rv: made sure test_013 always terminates

--- test/suite/test_013_reduced_loopnsmx16-wfv-foo2f8_TTrT.cpp
+++ test/suite/test_013_reduced_loopnsmx16-wfv-foo2f8_TTrT.cpp
@@ -1,19 +1,27 @@
 // test_072_loopnsmx16(float a, float b)
 // in search for a simplified version
 
+#include <algorithm>
+#include <cmath>
+
+float bound(float v) {
+  return std::min<float>(fabs(v), 400.0);
+}
+
 extern "C" float
 foo(float a, float b)
 {
-    float z, r, x = a + b;
-    float y =z=r= x * x - b;
+    float  r, x = a + b;
+    float y =r= x * x - b;
+    float z= fabs(a);
 
-    for (float i=3.2f; i<a ; i=i+2) {
+    for (float i=3.2f; i<bound(a) ; i=i+2000.0f) {
 
-X:    for (int j=0; j<b; ++j) {
+X:    for (int j=0; j<bound(b); j+=2000.0f) {
         z += y-i;
       }
 
-      for (int k=0; k<x; ++k) {
+      for (int k=0; k<bound(x); k += 5000) {
            z = z-3*a; // keep!
        }
 
