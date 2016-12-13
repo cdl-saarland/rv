cce188d6bfadc511af622d9bfddd28c6147be15a
Simon Moll <moll@cs.uni-saarland.de>
2016-12-13 15:52:43 +0100

    test_rv: migrated some more tests from wfvTestSuite

---
+++ test/suite/test_019_loopmx03-wfv-foo2f8_TTrT.cpp
@@ -0,0 +1,20 @@
+// test_031_loopmx03(float a, float b)
+
+extern "C" float
+foo(float a, float b)
+{
+    float x = a + b;
+    float y = x * x - b;
+    float z = y;
+
+    for (int i=0; i<100; ++i) {
+        z += x;
+        if (i > a) break;
+    }
+
+    z = z+x;
+    z = y-z;
+
+    return z;
+}
+
