118a4d2a409dc9b03a8171cd8e2875b2637cebe9
Simon Moll <moll@cs.uni-saarland.de>
2016-12-19 16:56:03 +0100

    test_024 :multilevel break test

---
+++ test/suite/test_024_2break-wfv-foo2f8_TTrT.cpp
@@ -0,0 +1,26 @@
+typedef float float4 __attribute__((ext_vector_type(4)));
+
+#include <cmath>
+
+extern "C"
+float
+foo(float a, float b) {
+  float x = fabs(a);
+  float y = fabs(b);
+
+  float z = a + b;
+
+  while (x > 0) {
+    x = x / 10.0f;
+    z = z + x;
+    while (y > 0) {
+      y = y / 42.0;
+      z = z - y * x;
+      if (z > 1000.0) {
+        goto D; // breaks out of both loops
+      }
+    }
+  }
+D:
+  return z;
+}
