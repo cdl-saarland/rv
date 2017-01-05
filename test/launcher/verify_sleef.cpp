13f3a3e44c9af2bd0e170cc09c9839c3d641f14c
Dominik Montada <dominik.montada@gmail.com>
2017-01-05 18:39:26 +0100

    test_25: test all sleef functions for inaccuracies

---
+++ test/launcher/verify_sleef.cpp
@@ -0,0 +1,49 @@
+/*
+ * foo_launcher.cpp
+ *
+ *  Created on: Jul 22, 2015
+ *      Author: Simon Moll
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+#include <cassert>
+
+#include "launcherTools.h"
+
+extern "C" float foo(int u, float t);
+extern "C" float8 foo_SIMD(int u, float8 t);
+
+int main(int argc, char ** argv) {
+  const uint vectorWidth = 8;
+  const uint numVectors = 100;
+
+  for (unsigned i = 0; i < numVectors; ++i) {
+    int a = i;
+    float b[8];
+
+    for (uint i = 0; i < vectorWidth; ++i) {
+      b[i] = (float) rand();
+    }
+
+    float8 rVec = foo_SIMD(a, *((float8*) &b));
+    float r[8];
+    toArray(rVec, r);
+
+    for (uint i = 0; i < vectorWidth; ++i) {
+      float expectedRes = foo(a, b[i]);
+
+      if (r[i] != expectedRes) {
+        std::cerr << "MISMATCH!\n";
+        std::cerr << i << " : a = " << a << " b = " << b[i] << " expected result " << expectedRes << " but was " << r[i] << "\n";
+        dumpArray(b, vectorWidth); std::cerr << "\n";
+        dumpArray(r, vectorWidth); std::cerr << "\n";
+        return -1;
+      }
+    }
+  }
+
+  return 0;
+}
