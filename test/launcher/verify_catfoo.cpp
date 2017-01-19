59f9cbb20f1273d6ec53448a7d296025b4b9ba91
Simon Moll <moll@cs.uni-saarland.de>
2017-01-19 14:37:57 +0100

    catfoo launcher

---
+++ test/launcher/verify_catfoo.cpp
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+#include <cassert>
+
+#include "launcherTools.h"
+
+// typedef float float4 __attribute__((ext_vector_type(4)));
+
+extern "C" float foo(int c, float * A);
+extern "C" float8 foo_SIMD(int c, float * A);
+
+int main(int argc, char ** argv) {
+  const uint vectorWidth = 8;
+  const uint numIters = 1;
+
+  float data[2048];
+
+  for (unsigned b = 0; b < numIters; ++b) {
+
+    for (int j = 0; j < 2048; ++j) {
+      data[j] = (float) rand();
+    }
+
+    // invoke SIMD
+    auto res = foo_SIMD(b * vectorWidth, data);
+
+    float aVec[vectorWidth];
+    toArray<float, float8>(res, aVec);
+
+    // invoke scalar
+    float aScalar[vectorWidth];
+    for (int j = 0; j < vectorWidth; ++j) {
+      aScalar[j] = foo(b*vectorWidth + j, data);
+    }
+
+    for (uint i = 0; i < vectorWidth; ++i) {
+      if (aScalar[i] != aVec[i]) {
+        std::cerr << "MISMATCH!\n";
+        std::cerr << i << " : s[i] = " << aScalar[i] << " v[i] = " << aVec[i] << "\n";
+        dumpArray(aScalar, vectorWidth); std::cerr << "\n";
+        dumpArray(aVec, vectorWidth); std::cerr << "\n";
+        return -1;
+      }
+    }
+  }
+
+  return 0;
+}
