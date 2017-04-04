5feb6c5eef75de29d82b35d807d3eabfa86c47fe
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2017-04-04 14:40:40 +0200

    Missing C source for intrinsics

---
+++ vecmath/extra/extradp.c
@@ -0,0 +1,39 @@
+#include <immintrin.h>
+#include <stdint.h>
+
+inline double as_double(int64_t x) {
+    union { int64_t i; double d; } u = { .i = x };
+    return u.d;
+}
+
+#ifdef ENABLE_AVX
+__m256d xfabs(__m256d x) {
+    return _mm256_and_pd(x, _mm256_set1_pd(as_double(0x7FFFFFFFFFFFFFFFll)));
+}
+__m256d xcopysign(__m256d x, __m256d y) {
+    __m256d s = _mm256_and_pd(y, _mm256_set1_pd(as_double(0x8000000000000000)));
+    return _mm256_or_pd(_mm256_and_pd(x, _mm256_set1_pd(as_double(0x7FFFFFFFFFFFFFFF))), s);
+}
+__m256d xfmin(__m256d x, __m256d y) {
+    return _mm256_blendv_pd(y, x, _mm256_cmp_pd(x, y, 1));
+}
+__m256d xfmax(__m256d x, __m256d y) {
+    return _mm256_blendv_pd(y, x, _mm256_cmp_pd(x, y, 6));
+}
+#endif
+
+#ifdef ENABLE_SSE41
+__m128d xfabs(__m128d x) {
+    return _mm_and_pd(x, _mm_set1_pd(as_double(0x7FFFFFFFFFFFFFFFll)));
+}
+__m128d xcopysign(__m128d x, __m128d y) {
+    __m128d s = _mm_and_pd(y, _mm_set1_pd(as_double(0x8000000000000000)));
+    return _mm_or_pd(_mm_and_pd(x, _mm_set1_pd(as_double(0x7FFFFFFFFFFFFFFF))), s);
+}
+__m128d xfmin(__m128d x, __m128d y) {
+    return _mm_blendv_pd(y, x, _mm_cmplt_pd(x, y));
+}
+__m128d xfmax(__m128d x, __m128d y) {
+    return _mm_blendv_pd(y, x, _mm_cmpnle_pd(x, y));
+}
+#endif
---
+++ vecmath/extra/extrasp.c
@@ -0,0 +1,39 @@
+#include <immintrin.h>
+#include <stdint.h>
+
+inline float as_float(int x) {
+    union { int i; float f; } u = { .i = x };
+    return u.f;
+}
+
+#ifdef ENABLE_AVX
+__m256 xfabsf(__m256 x) {
+    return _mm256_and_ps(x, _mm256_set1_ps(as_float(0x7FFFFFFF)));
+}
+__m256 xcopysignf(__m256 x, __m256 y) {
+    __m256 s = _mm256_and_ps(y, _mm256_set1_ps(as_float(0x80000000)));
+    return _mm256_or_ps(_mm256_and_ps(x, _mm256_set1_ps(as_float(0x7FFFFFFF))), s);
+}
+__m256 xfminf(__m256 x, __m256 y) {
+    return _mm256_blendv_ps(y, x, _mm256_cmp_ps(x, y, 1));
+}
+__m256 xfmaxf(__m256 x, __m256 y) {
+    return _mm256_blendv_ps(y, x, _mm256_cmp_ps(x, y, 6));
+}
+#endif
+
+#ifdef ENABLE_SSE41
+__m128 xfabsf(__m128 x) {
+    return _mm_and_ps(x, _mm_set1_ps(as_float(0x7FFFFFFF)));
+}
+__m128 xcopysignf(__m128 x, __m128 y) {
+    __m128 s = _mm_and_ps(y, _mm_set1_ps(as_float(0x80000000)));
+    return _mm_or_ps(_mm_and_ps(x, _mm_set1_ps(as_float(0x7FFFFFFF))), s);
+}
+__m128 xfminf(__m128 x, __m128 y) {
+    return _mm_blendv_ps(y, x, _mm_cmplt_ps(x, y));
+}
+__m128 xfmaxf(__m128 x, __m128 y) {
+    return _mm_blendv_ps(y, x, _mm_cmpnle_ps(x, y));
+}
+#endif
