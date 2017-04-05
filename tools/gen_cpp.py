417f4dbb64176da723e40fe9719f68efac9a7c26
Simon Moll <moll@cs.uni-saarland.de>
2017-04-05 09:30:26 +0200

    bld: create generator files from vecmath bc using gen_cpp

--- tools/gen_cpp.py
+++ tools/gen_cpp.py
@@ -2,12 +2,29 @@
 import sys
 
 cppFileName = sys.argv[1]
-bcFile = sys.argv[2]
+bufferName = sys.argv[2]
+bcFile = sys.argv[3]
+
+
+def encodeByte(byte):
+  return "{}".format(byte)
+
+  if byte == '\\':
+    return "\\\\"
+  elif byte == '\"':
+    return "\\\""
+  elif byte == '\n':
+    return "\\n"
+  elif byte == '\t':
+    return "\\t"
+  else:
+    return byte
+
 
 stream = open(bcFile, 'br')
 with open(cppFileName, 'w') as out:
   # prologue
-  out.write("const unsigned char * buffer = (unsigned char[]){") 
+  out.write("#include<string>\nextern const unsigned char {}_Buffer[] = {}".format(bufferName, "{")) 
 
   # transcode file
   later = False
@@ -15,13 +32,13 @@ with open(cppFileName, 'w') as out:
   while True:
     byte = stream.read(1)
     if not byte:
-      raise SystemExit
+      break
 
     if later:
       out.write(',')
 
     later = True
-    out.write("{}".format(byte[0]))
+    out.write("0x{:02X}".format(byte[0]))
 
   # epilogue
-  out.write("};\n")
+  out.write("{0};\nextern const size_t {1}_BufferLen = sizeof({1}_Buffer);\n".format("}", bufferName))
