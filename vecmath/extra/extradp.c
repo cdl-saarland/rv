9a1572d80802ec212478ef5c5901a4f68ce095a5
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2017-05-26 18:29:54 +0200

    Add always_inline attributes to small vecmath functions

--- vecmath/extra/extradp.c
+++ vecmath/extra/extradp.c
@@ -7,33 +7,33 @@ inline double as_double(int64_t x) {
 }
 
 #ifdef ENABLE_AVX
-__m256d xfabs(__m256d x) {
+__m256d xfabs(__m256d x) __attribute__((always_inline)) {
     return _mm256_and_pd(x, _mm256_set1_pd(as_double(0x7FFFFFFFFFFFFFFFll)));
 }
-__m256d xcopysign(__m256d x, __m256d y) {
+__m256d xcopysign(__m256d x, __m256d y) __attribute__((always_inline)) {
     __m256d s = _mm256_and_pd(y, _mm256_set1_pd(as_double(0x8000000000000000)));
     return _mm256_or_pd(_mm256_and_pd(x, _mm256_set1_pd(as_double(0x7FFFFFFFFFFFFFFF))), s);
 }
-__m256d xfmin(__m256d x, __m256d y) {
+__m256d xfmin(__m256d x, __m256d y) __attribute__((always_inline)) {
     return _mm256_min_pd(x, y);
 }
-__m256d xfmax(__m256d x, __m256d y) {
+__m256d xfmax(__m256d x, __m256d y) __attribute__((always_inline)) {
     return _mm256_max_pd(x, y);
 }
 #endif
 
 #ifdef ENABLE_SSE41
-__m128d xfabs(__m128d x) {
+__m128d xfabs(__m128d x) __attribute__((always_inline)) {
     return _mm_and_pd(x, _mm_set1_pd(as_double(0x7FFFFFFFFFFFFFFFll)));
 }
-__m128d xcopysign(__m128d x, __m128d y) {
+__m128d xcopysign(__m128d x, __m128d y) __attribute__((always_inline)) {
     __m128d s = _mm_and_pd(y, _mm_set1_pd(as_double(0x8000000000000000)));
     return _mm_or_pd(_mm_and_pd(x, _mm_set1_pd(as_double(0x7FFFFFFFFFFFFFFF))), s);
 }
-__m128d xfmin(__m128d x, __m128d y) {
+__m128d xfmin(__m128d x, __m128d y) __attribute__((always_inline)) {
     return _mm_min_pd(x, y);
 }
-__m128d xfmax(__m128d x, __m128d y) {
+__m128d xfmax(__m128d x, __m128d y) __attribute__((always_inline)) {
     return _mm_max_pd(x, y);
 }
 #endif
