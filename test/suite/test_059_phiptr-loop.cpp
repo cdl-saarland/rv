b53907ac777a84c84a543619dc108f2ce15b3034
Simon Moll <moll@cs.uni-saarland.de>
2017-06-27 12:42:15 +0200

    test_059: leaking uni ptr

---
+++ test/suite/test_059_phiptr-loop.cpp
@@ -0,0 +1,17 @@
+// LoopHint: 0, LaunchCode: fooABn
+
+extern "C"
+void
+foo(float *A, float * B, int n) {
+  for (int i = 0; i < n/2; ++i) {
+
+    float * X = A;
+    for (int j = 0; j < 63; ++j) {
+      if (B[j] < -2.0f) { ++X; }
+      if (B[j] > 10.0f) break;
+    }
+
+    X[2 * i] = B[i];
+    X[2 * i + 1] = B[i];
+  }
+}
