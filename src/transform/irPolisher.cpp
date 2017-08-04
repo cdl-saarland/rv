5d675c0c7eb2e81415250d337a9a47ce6f22c8bf
Arsene Perard-Gayot <perard@cg.uni-saarland.de>
2017-08-04 16:20:33 +0200

    IRPolisher: Prevent double deletion of polished instructions

--- src/transform/irPolisher.cpp
+++ src/transform/irPolisher.cpp
@@ -38,7 +38,6 @@ using namespace llvm;
 using namespace rv;
 
 
-
 void IRPolisher::enqueueInst(llvm::Instruction* inst, unsigned bitWidth) {
   ExtInst extInst(inst, bitWidth);
   if (visitedInsts.find(extInst) == visitedInsts.end())
@@ -59,22 +58,10 @@ bool IRPolisher::canReplaceInst(llvm::Instruction *inst, unsigned& bitWidth) {
   auto vecLen = instTy->getVectorNumElements();
   if (vecLen != 2 && vecLen != 4 && vecLen != 8) return false;
 
-  // Bitwidth the second operand (2nd compared value or one of the select branches)
-  bitWidth = inst->getOperand(1)->getType()->getScalarSizeInBits();
+  bitWidth = inst->getOperand(0)->getType()->getScalarSizeInBits();
   if (vecLen * bitWidth != 128 &&
       vecLen * bitWidth != 256)
     return false;
-
-  // Try to not to modify min/max patterns
-  for (auto user : inst->users()) {
-    if (auto selectInst = dyn_cast<SelectInst>(user)) {
-      Value *left, *right;
-      Instruction::CastOps castOp;
-      auto selectPattern = matchSelectPattern(selectInst, left, right, &castOp);
-      if (SelectPatternResult::isMinOrMax(selectPattern.Flavor))
-        return false;
-    }
-  }
 
   return true;
 }
@@ -127,36 +114,35 @@ Value *IRPolisher::lowerIntrinsicCall(llvm::CallInst* callInst) {
 Value *IRPolisher::replaceCmpInst(IRBuilder<> &builder, llvm::CmpInst *cmpInst, unsigned bitWidth) {
   auto left  = cmpInst->getOperand(0);
   auto right = cmpInst->getOperand(1);
+  auto boolCmp  = isBooleanVector(left->getType());
+  auto newLeft  = boolCmp ? getMaskForValueOrInst(builder, left , bitWidth) : left;
+  auto newRight = boolCmp ? getMaskForValueOrInst(builder, right, bitWidth) : right;
 
   // Optimizations:
   // cmp eq <n x i1> %x, false => not %x
   // cmp ne <n x i1> %x, true  => not %x
   // cmp eq <n x i1> %x, true  => %x
   // cmp ne <n x i1> %x, false => %x
-  auto boolCmp = isBooleanVector(left->getType());
   auto pred = cmpInst->getPredicate();
   if (boolCmp && (pred == CmpInst::ICMP_EQ || pred == CmpInst::ICMP_NE)) {
     if (auto leftCst = dyn_cast<Constant>(left)) {
       if ((leftCst->isAllOnesValue() && pred == CmpInst::ICMP_EQ) ||
           (leftCst->isZeroValue()    && pred == CmpInst::ICMP_NE)) {
-        return getMaskForValueOrInst(builder, right, bitWidth);
+        return newRight;
       } else if ((leftCst->isAllOnesValue() && pred == CmpInst::ICMP_NE) ||
                  (leftCst->isZeroValue()    && pred == CmpInst::ICMP_EQ)) {
-        return builder.CreateNot(getMaskForValueOrInst(builder, right, bitWidth));
+        return builder.CreateNot(newRight);
       }
     } else if (auto rightCst = dyn_cast<Constant>(right)) {
       if ((rightCst->isAllOnesValue() && pred == CmpInst::ICMP_EQ) ||
           (rightCst->isZeroValue()    && pred == CmpInst::ICMP_NE)) {
-        return getMaskForValueOrInst(builder, left, bitWidth);
+        return newLeft;
       } else if ((rightCst->isAllOnesValue() && pred == CmpInst::ICMP_EQ) ||
                  (rightCst->isZeroValue()    && pred == CmpInst::ICMP_NE)) {
-        return builder.CreateNot(getMaskForValueOrInst(builder, left, bitWidth));
+        return builder.CreateNot(newLeft);
       }
     }
   }
-
-  auto newLeft  = boolCmp ? getMaskForValueOrInst(builder, left,  bitWidth) : left;
-  auto newRight = boolCmp ? getMaskForValueOrInst(builder, right, bitWidth) : right;
 
   auto newLeftTy = newLeft->getType();
   auto vecLen    = newLeftTy->getVectorNumElements();
@@ -203,57 +189,63 @@ Value *IRPolisher::replaceCmpInst(IRBuilder<> &builder, llvm::CmpInst *cmpInst, 
     }
   }
 
-  return cmpInst->isFPPredicate()
+  auto newCmp = cmpInst->isFPPredicate()
     ? builder.CreateFCmp(cmpInst->getPredicate(), newLeft, newRight)
     : builder.CreateICmp(cmpInst->getPredicate(), newLeft, newRight);
+  return getMaskForValue(builder, newCmp, bitWidth);
 }
 
 Value *IRPolisher::replaceSelectInst(IRBuilder<> &builder, llvm::SelectInst *selectInst, unsigned bitWidth) {
-  // If the select is part of a min/max pattern, do not replace it
-  {
+  // Get a mask from the condition (could come from ANDs)
+  auto s1 = selectInst->getOperand(1);
+  auto s2 = selectInst->getOperand(2);
+  auto boolSelect = isBooleanVector(selectInst->getType());
+  auto condMask = getMaskForValueOrInst(builder, selectInst->getOperand(0), bitWidth);
+  auto newS1 = boolSelect ? getMaskForValueOrInst(builder, s1, bitWidth) : s1;
+  auto newS2 = boolSelect ? getMaskForValueOrInst(builder, s2, bitWidth) : s2;
+
+  auto newS1Ty = newS1->getType();
+  auto opBitWidth = newS1Ty->getScalarSizeInBits();
+  auto vecLen = newS1Ty->getVectorNumElements();
+
+  // If the select is part of a min/max pattern, try to keep the pattern intact
+  auto cmpInst = dyn_cast<CmpInst>(selectInst->getOperand(0));
+  if (cmpInst) {
     Value *left, *right;
     Instruction::CastOps castOp;
     auto selectPattern = matchSelectPattern(selectInst, left, right, &castOp);
-    if (SelectPatternResult::isMinOrMax(selectPattern.Flavor))
-      return builder.Insert(selectInst->clone());
+    if (SelectPatternResult::isMinOrMax(selectPattern.Flavor)) {
+      auto cmpClone = builder.Insert(cmpInst->clone());
+      auto selectClone = builder.Insert(selectInst->clone());
+      selectClone->setOperand(0, cmpClone);
+      return selectClone;
+    }
   }
-
-  auto cond = selectInst->getOperand(0);
-  auto s1   = selectInst->getOperand(1);
-  auto s2   = selectInst->getOperand(2);
 
   // Optimization: if the select is using a NOT,
   // then invert operands and use original (non-NOT) value.
   bool invertOps = false;
-  if (BinaryOperator::isNot(cond)) {
+  if (BinaryOperator::isNot(condMask)) {
     invertOps = true;
-    cond = BinaryOperator::getNotArgument(cond);
+    condMask = BinaryOperator::getNotArgument(condMask);
   }
-  if (invertOps) std::swap(s1, s2);
+  if (invertOps) std::swap(newS1, newS2);
 
   // Optimizations:
   // select %x true false  -> cond
   // select %x false true  -> not cond
-  auto boolSelect = isBooleanVector(s1->getType());
   if (boolSelect) {
-    auto s1Cst = dyn_cast<Constant>(s1);
-    auto s2Cst = dyn_cast<Constant>(s2);
+    auto s1Cst = dyn_cast<Constant>(newS1);
+    auto s2Cst = dyn_cast<Constant>(newS2);
     if (s1Cst && s2Cst) {
       if (s1Cst->isAllOnesValue() && s2Cst->isZeroValue())
-        return getMaskForValueOrInst(builder, cond, bitWidth);
+        return condMask;
       if (s1Cst->isZeroValue() && s2Cst->isAllOnesValue())
-        return builder.CreateNot(getMaskForValueOrInst(builder, cond, bitWidth));
+        return builder.CreateNot(condMask);
     }
   }
 
-  // Get a mask from the condition (could come from ANDs)
-  auto condMask = getMaskForValueOrInst(builder, cond, bitWidth);
-  auto newS1 = boolSelect ? getMaskForValueOrInst(builder, s1, bitWidth) : s1;
-  auto newS2 = boolSelect ? getMaskForValueOrInst(builder, s2, bitWidth) : s2;
-
   // Replace with blendvps/pd when possible, otherwise fall back to a LLVM select
-  auto opBitWidth = newS1->getType()->getScalarSizeInBits();
-  auto vecLen = cond->getType()->getVectorNumElements();
   if ((opBitWidth == bitWidth) &&
       (vecLen == 2 || vecLen == 4 || vecLen == 8) &&
       (vecLen * bitWidth == 256 || vecLen * bitWidth == 128)) {
@@ -460,16 +452,24 @@ bool IRPolisher::polish() {
   }
 
   // Remove original versions of transformed instructions
-  for (auto& extInst : visitedInsts) {
+  for (auto &extInst : visitedInsts) {
     auto inst = extInst.first.inst;
-#ifndef NDEBUG
+
+    // The same instruction might have been replaced with 2 polished
+    // instructions (32 and 64 bits), and we must only remove it once
+    if (extInst.first.bitWidth == 64 && visitedInsts.count(ExtInst(inst, 32))) continue;
+
+#ifdef RV_DEBUG
     // Check that all the users of this instruction have been processed before removing it
     for (auto user : inst->users()) {
       if (auto userInst = dyn_cast<Instruction>(user)) {
-        assert(visitedInsts.find(ExtInst(userInst, extInst.first.bitWidth)) != visitedInsts.end());
+        // This instruction might be used with a different bit width in different contexts
+        assert(visitedInsts.count(ExtInst(userInst, 32)) != 0 ||
+               visitedInsts.count(ExtInst(userInst, 64)) != 0);
       }
     }
 #endif
+
     inst->replaceAllUsesWith(UndefValue::get(inst->getType()));
     inst->eraseFromParent();
   }
