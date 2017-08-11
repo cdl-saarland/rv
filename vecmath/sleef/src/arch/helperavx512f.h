25f52269d8ecd8c4d74cf78489273eb26f169724
Simon Moll <moll@cs.uni-saarland.de>
2017-08-11 11:22:38 +0200

    vecmath,build: build SLEEF avx512 library

--- vecmath/sleef/src/arch/helperavx512f.h
+++ vecmath/sleef/src/arch/helperavx512f.h
@@ -254,8 +254,8 @@ static INLINE vfloat vgetexp_vf_vf(vfloat d) { return _mm512_getexp_ps(d); }
 static INLINE vdouble vgetmant_vd_vd(vdouble d) { return _mm512_getmant_pd(d, _MM_MANT_NORM_p75_1p5, _MM_MANT_SIGN_nan); }
 static INLINE vfloat vgetmant_vf_vf(vfloat d) { return _mm512_getmant_ps(d, _MM_MANT_NORM_p75_1p5, _MM_MANT_SIGN_nan); }
 
-static INLINE vdouble vfixup_vd_vd_vd_vi2_i(vdouble a, vdouble b, vint2 c, int imm) { return _mm512_fixupimm_pd(a, b, c, imm); }
-static INLINE vfloat vfixup_vf_vf_vf_vi2_i(vfloat a, vfloat b, vint2 c, int imm) { return _mm512_fixupimm_ps(a, b, c, imm); }
+#define vfixup_vd_vd_vd_vi2_i(A,B,C,IMM) _mm512_fixupimm_pd(A, B, C, IMM)
+#define vfixup_vf_vf_vf_vi2_i(A,B,C,IMM) _mm512_fixupimm_ps(A, B, C, IMM)
 
 #if defined(_MSC_VER)
 // This function is needed when debugging on MSVC.
