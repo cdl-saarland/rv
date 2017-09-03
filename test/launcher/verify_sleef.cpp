0bc44f7b6d816407456ca3bb1528b761ed00488c
Simon Moll <moll@cs.uni-saarland.de>
2017-09-03 16:22:15 +0200

    made SLEEF launcher tollerant to slight fp deviations

--- test/launcher/verify_sleef.cpp
+++ test/launcher/verify_sleef.cpp
@@ -10,11 +10,29 @@
 #include <iostream>
 
 #include <cassert>
+#include <cmath>
+#include <limits>
 
 #include "launcherTools.h"
 
 extern "C" float foo(int u, float t);
 extern "C" float8 foo_SIMD(int u, float8 t);
+
+template<typename TReal>
+static bool isWithinPrecisionInterval(TReal a, TReal b, unsigned int interval_size = 1)
+{
+  if (!std::isfinite(a) && !std::isfinite(b)) return true;
+
+    TReal min_a = a - (a - std::nextafter(a, std::numeric_limits<TReal>::lowest())) * interval_size;
+    TReal max_a = a + (std::nextafter(a, std::numeric_limits<TReal>::max()) - a) * interval_size;
+
+    return min_a <= b && max_a >= b;
+}
+
+static inline bool IsClose(float a, float b) {
+  const unsigned interval_size = 2;
+  return isWithinPrecisionInterval(a, b, interval_size);
+}
 
 int main(int argc, char ** argv) {
   const uint vectorWidth = 8;
@@ -32,12 +50,13 @@ int main(int argc, char ** argv) {
     float r[8];
     toArray(rVec, r);
 
+
     for (uint i = 0; i < vectorWidth; ++i) {
       float expectedRes = foo(a, b[i]);
 
-      if (r[i] != expectedRes) {
+      if (!IsClose(r[i], expectedRes)) {
         std::cerr << "MISMATCH!\n";
-        std::cerr << i << " : a = " << a << " b = " << b[i] << " expected result " << expectedRes << " but was " << r[i] << "\n";
+        std::cerr << i << " : a = " << a << " b = " << b[i] << " expected result (close to) " << expectedRes << " but was " << r[i] << "\n";
         dumpArray(b, vectorWidth); std::cerr << "\n";
         dumpArray(r, vectorWidth); std::cerr << "\n";
         return -1;
