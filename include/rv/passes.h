ec4eedabe73f5df37f56478b454961c22004cde7
Simon Moll <moll@cs.uni-saarland.de>
2018-05-03 16:42:29 +0200

    opt: -rv enables both WFV and outer loop vec.

--- include/rv/passes.h
+++ include/rv/passes.h
@@ -18,11 +18,17 @@ namespace rv {
   // Controlled Node Splitting (Irreducible loop normalization)
   llvm::FunctionPass *createCNSPass();
 
+  // add normalization passes required by RV (BEFORE)
+  void addPreparatoryPasses(llvm::legacy::PassManagerBase & PM);
+
   // add RV's outer loop vectorizer and required passes to @PM
   void addOuterLoopVectorizer(llvm::legacy::PassManagerBase & PM, Config config = Config());
 
   // add RV's whole function and required passes to @PM
   void addWholeFunctionVectorizer(llvm::legacy::PassManagerBase & PM);
+
+  // add cleanup passes to run after RV (AFTER)
+  void addCleanupPasses(llvm::legacy::PassManagerBase & PM);
 } // namespace rv
 
 
--- src/passes.cpp
+++ src/passes.cpp
@@ -2,6 +2,7 @@
 
 #include "llvm/Transforms/Utils.h"
 #include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/AggressiveInstCombine/AggressiveInstCombine.h"
 #include "llvm/Transforms/IPO/AlwaysInliner.h"
 #include "rv/transform/loopExitCanonicalizer.h"
 
@@ -9,8 +10,15 @@ using namespace llvm;
 
 namespace rv {
 
-static void
-AddCleanupPasses(legacy::PassManagerBase & PM) {
+void
+addPreparatoryPasses(legacy::PassManagerBase & PM) {
+   PM.add(createLoopSimplifyPass());
+   PM.add(createLCSSAPass());
+   PM.add(createLoopExitCanonicalizerPass()); // required for divLoopTrans
+}
+
+void
+addCleanupPasses(legacy::PassManagerBase & PM) {
    // post rv cleanup
    PM.add(createAlwaysInlinerLegacyPass());
    PM.add(createAggressiveInstCombinerPass());
@@ -20,22 +28,12 @@ AddCleanupPasses(legacy::PassManagerBase & PM) {
 void
 addOuterLoopVectorizer(legacy::PassManagerBase & PM, Config config) {
    // PM.add(rv::createCNSPass());
-   PM.add(createLoopSimplifyPass());
-   PM.add(createLCSSAPass());
-   PM.add(createLoopExitCanonicalizerPass()); // required for divLoopTrans
    PM.add(rv::createLoopVectorizerPass());
-
-   AddCleanupPasses(PM);
 }
 
 
 void addWholeFunctionVectorizer(llvm::legacy::PassManagerBase & PM) {
-   PM.add(createLoopSimplifyPass());
-  PM.add(createLCSSAPass());
-  PM.add(createLoopExitCanonicalizerPass()); // required for divLoopTrans
   PM.add(rv::createWFVPass());
-
-  AddCleanupPasses(PM);
 }
 
 }
