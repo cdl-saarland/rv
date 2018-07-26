879effee40e98ca2548291c97bbdc9985f97e285
Simon Moll <moll@cs.uni-saarland.de>
2018-07-26 17:50:44 +0200

    loopVec: fixed handling of loops with unbounded distance

--- include/rv/transform/LoopVectorizer.h
+++ include/rv/transform/LoopVectorizer.h
@@ -59,6 +59,7 @@ public:
   /// Register all analyses and transformation required.
   void getAnalysisUsage(llvm::AnalysisUsage &AU) const override;
 
+  static const int ParallelDistance = std::numeric_limits<int>::max();
 private:
   Config config;
 
@@ -87,11 +88,10 @@ private:
   int getTripCount(llvm::Loop &L);
   // return the annotated vector width
   // return -1, if unspecified
-  int getVectorWidth(llvm::Loop &L);
+  int getAnnotatedVectorWidth(llvm::Loop &L);
 
   // minimal distance between dependent loop trips
   // returns ParallelDistance for fully parallel loops
-  const int ParallelDistance = std::numeric_limits<int>::max();
   int getDependenceDistance(llvm::Loop & L);
 
   // the trip count of the loop is always a multiple of this value
