712d96b6e8a1fde2ab8290bafc95e2bde6c2f056
Simon Moll <moll@cs.uni-saarland.de>
2018-11-27 13:16:58 +0100

    NFC. vectorizationInfo autoformat

--- include/rv/vectorizationInfo.h
+++ include/rv/vectorizationInfo.h
@@ -1,4 +1,4 @@
-//===- vectorizationInfo.h -----------------------------===//
+//===-------------------- vectorizationInfo.h ----------------------------===//
 //
 //                     The Region Vectorizer
 //
@@ -10,146 +10,149 @@
 #define INCLUDE_RV_VECTORIZATIONINFO_H_
 
 namespace llvm {
-  class LLVMContext;
-  class BasicBlock;
-  class Instruction;
-  class Value;
-}
+class LLVMContext;
+class BasicBlock;
+class Instruction;
+class Value;
+} // namespace llvm
 
-
+#include "region/Region.h"
 #include "rv/shape/vectorShape.h"
 #include "rv/vectorMapping.h"
-#include "region/Region.h"
 
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/IR/DataLayout.h"
 #include "llvm/IR/ValueHandle.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/DataLayout.h"
-#include "llvm/Analysis/LoopInfo.h"
 
-#include <unordered_map>
 #include <set>
+#include <unordered_map>
 
-namespace rv
-{
+namespace rv {
 
 class Region;
 
-// provides vectorization information (vector shapes, block predicates) for a function
-class VectorizationInfo
-{
-    const llvm::DataLayout & DL;
+// provides vectorization information (vector shapes, block predicates) for a
+// function
+class VectorizationInfo {
+  const llvm::DataLayout &DL;
 
   // analysis context
-    Region& region;
-    VectorMapping mapping;
+  Region &region;
+  VectorMapping mapping;
 
-    // value, argument and instruction shapes
-    std::unordered_map<const llvm::Value*, VectorShape> shapes;
+  // value, argument and instruction shapes
+  std::unordered_map<const llvm::Value *, VectorShape> shapes;
 
   // detected divergent loops
-    std::set<const llvm::Loop*> mDivergentLoops;
+  std::set<const llvm::Loop *> mDivergentLoops;
 
   // basic block properties // TODO fuse into struct
-    // materialized basic block predicates
-    std::unordered_map<const llvm::BasicBlock*, llvm::TrackingVH<llvm::Value>> predicates;
-    // whether the block is the exit of a divergent loop exit
-    std::set<const llvm::BasicBlock*> DivergentLoopExits;
-    // wheterh the block is a join point of disjoint paths from a varying branch
-    std::set<const llvm::BasicBlock*> JoinDivergentBlocks;
-    // wheterh the block will receive a non-uniform predicate
-    std::set<const llvm::BasicBlock*> VaryingPredicateBlocks;
+  // materialized basic block predicates
+  std::unordered_map<const llvm::BasicBlock *, llvm::TrackingVH<llvm::Value>>
+      predicates;
+  // whether the block is the exit of a divergent loop exit
+  std::set<const llvm::BasicBlock *> DivergentLoopExits;
+  // whether the block is a join point of disjoint paths from a varying branch
+  std::set<const llvm::BasicBlock *> JoinDivergentBlocks;
+  // whether the block will receive a non-uniform predicate
+  std::set<const llvm::BasicBlock *> VaryingPredicateBlocks;
 
   // fixed shapes (will be preserved through VA)
-    std::set<const llvm::Value*> pinned;
+  std::set<const llvm::Value *> pinned;
 
 public:
-    VectorizationInfo(Region & region, VectorMapping _mapping);
-    VectorizationInfo(llvm::Function& parentFn, unsigned vectorWidth, Region& region);
+  VectorizationInfo(Region &region, VectorMapping _mapping);
+  VectorizationInfo(llvm::Function &parentFn, unsigned vectorWidth,
+                    Region &region);
 
+  const llvm::DataLayout &getDataLayout() const;
+  const VectorMapping &getMapping() const { return mapping; }
+  size_t getVectorWidth() const { return mapping.vectorWidth; }
 
-    const llvm::DataLayout & getDataLayout() const;
-    const VectorMapping& getMapping() const { return mapping; }
-    size_t getVectorWidth() const { return mapping.vectorWidth; }
+  // region related
+  Region &getRegion() const { return region; }
+  bool inRegion(const llvm::Instruction &inst) const;
+  bool inRegion(const llvm::BasicBlock &block) const;
+  llvm::BasicBlock &getEntry() const;
 
-    // region related
-    Region& getRegion() const { return region; }
-    bool inRegion(const llvm::Instruction & inst) const;
-    bool inRegion(const llvm::BasicBlock & block) const;
-    llvm::BasicBlock & getEntry() const;
+  // disjoin path divergence
+  bool isJoinDivergent(const llvm::BasicBlock &JoinBlock) const {
+    return JoinDivergentBlocks.count(&JoinBlock);
+  }
+  bool addJoinDivergentBlock(const llvm::BasicBlock &JoinBlock) {
+    return JoinDivergentBlocks.insert(&JoinBlock).second;
+  }
 
-    // disjoin path divergence
-    bool isJoinDivergent(const llvm::BasicBlock & JoinBlock) const { return JoinDivergentBlocks.count(&JoinBlock); }
-    bool addJoinDivergentBlock(const llvm::BasicBlock& JoinBlock) { return JoinDivergentBlocks.insert(&JoinBlock).second; }
+  // loop divergence
+  bool addDivergentLoop(const llvm::Loop &divLoop);
+  void removeDivergentLoop(const llvm::Loop &divLoop);
+  bool isDivergentLoop(const llvm::Loop &loop) const;
+  bool isDivergentLoopTopLevel(const llvm::Loop &loop) const;
 
-    // loop divergence
-    bool addDivergentLoop(const llvm::Loop & divLoop);
-    void removeDivergentLoop(const llvm::Loop & divLoop);
-    bool isDivergentLoop(const llvm::Loop& loop) const;
-    bool isDivergentLoopTopLevel(const llvm::Loop& loop) const;
+  // loop exit divergence
+  bool isDivergentLoopExit(const llvm::BasicBlock &block) const;
+  bool isKillExit(const llvm::BasicBlock &block) const {
+    return !isDivergentLoopExit(block);
+  }
+  bool addDivergentLoopExit(const llvm::BasicBlock &block);
+  void removeDivergentLoopExit(const llvm::BasicBlock &block);
 
-    // loop exit divergence
-    bool isDivergentLoopExit(const llvm::BasicBlock & block) const;
-    bool isKillExit(const llvm::BasicBlock & block) const { return !isDivergentLoopExit(block); }
-    bool addDivergentLoopExit(const llvm::BasicBlock& block);
-    void removeDivergentLoopExit(const llvm::BasicBlock& block);
+  /// Disable recomputation of this value's shape and make it effectvely final
+  const decltype(pinned) &pinned_values() const { return pinned; }
+  void setPinned(const llvm::Value &);
+  void setPinnedShape(const llvm::Value &v, VectorShape shape) {
+    setPinned(v);
+    setVectorShape(v, shape);
+  }
+  bool isPinned(const llvm::Value &) const;
 
+  // vector shape
+  // get the shape of @val observed at @observerBlock. This will be varying if
+  // @val is defined in divergent loop.
+  VectorShape getObservedShape(const llvm::LoopInfo &LI,
+                               const llvm::BasicBlock &observerBlock,
+                               const llvm::Value &val) const;
 
-    /// Disable recomputation of this value's shape and make it effectvely final
-    const decltype(pinned) & pinned_values() const { return pinned; }
-    void setPinned(const llvm::Value&);
-    void setPinnedShape(const llvm::Value& v, VectorShape shape) {
-      setPinned(v);
-      setVectorShape(v, shape);
-    }
-    bool isPinned(const llvm::Value&) const;
+  // get the shape of @val observerd in the defining block of @val (if it is an
+  // instruction).
+  VectorShape getVectorShape(const llvm::Value &val) const;
+  bool hasKnownShape(const llvm::Value &val) const;
 
+  void setVectorShape(const llvm::Value &val, VectorShape shape);
+  void dropVectorShape(const llvm::Value &val);
 
-    // vector shape
-    // get the shape of @val observed at @observerBlock. This will be varying if @val is defined in divergent loop.
-    VectorShape getObservedShape(const llvm::LoopInfo & LI, const llvm::BasicBlock & observerBlock, const llvm::Value & val) const;
+  bool isTemporalDivergent(const llvm::LoopInfo &LI,
+                           const llvm::BasicBlock &ObservingBlock,
+                           const llvm::Value &Val) const;
 
-    // get the shape of @val observerd in the defining block of @val (if it is an instruction).
-    VectorShape getVectorShape(const llvm::Value& val) const;
-    bool hasKnownShape(const llvm::Value& val) const;
+  // tentative block predicate shapes (whether the basic block predicate will be
+  // varying or uniform)
+  bool hasVaryingPredicate(const llvm::BasicBlock &BB) const;
+  void addVaryingPredicateFlag(const llvm::BasicBlock &);
+  void removeVaryingPredicatelag(const llvm::BasicBlock &);
 
-    void setVectorShape(const llvm::Value& val, VectorShape shape);
-    void dropVectorShape(const llvm::Value& val);
+  // actual basic block predicates
+  llvm::Value *getPredicate(const llvm::BasicBlock &block) const;
+  void setPredicate(const llvm::BasicBlock &block, llvm::Value &predicate);
+  void dropPredicate(const llvm::BasicBlock &block);
+  void remapPredicate(llvm::Value &dest, llvm::Value &old);
 
-    bool isTemporalDivergent(const llvm::LoopInfo & LI,
-                             const llvm::BasicBlock &ObservingBlock,
-                             const llvm::Value &Val) const;
+  // print
+  void dump() const;
+  void print(llvm::raw_ostream &out) const;
+  void dump(const llvm::Value *val) const;
+  void print(const llvm::Value *val, llvm::raw_ostream &) const;
+  void printBlockInfo(const llvm::BasicBlock &block, llvm::raw_ostream &) const;
+  void dumpBlockInfo(const llvm::BasicBlock &block) const;
+  void printArguments(llvm::raw_ostream &) const;
+  void dumpArguments() const;
 
-
-    // tentative block predicate shapes (whether the basic block predicate will be varying or uniform)
-    bool hasVaryingPredicate(const llvm::BasicBlock & BB) const;
-    void addVaryingPredicateFlag(const llvm::BasicBlock & );
-    void removeVaryingPredicatelag(const llvm::BasicBlock & );
-
-
-    // actual basic block predicates
-    llvm::Value* getPredicate(const llvm::BasicBlock& block) const;
-    void setPredicate(const llvm::BasicBlock& block, llvm::Value& predicate);
-    void dropPredicate(const llvm::BasicBlock& block);
-    void remapPredicate(llvm::Value& dest, llvm::Value& old);
-
-
-    // print
-    void dump() const;
-    void print(llvm::raw_ostream & out) const;
-    void dump(const llvm::Value * val) const;
-    void print(const llvm::Value * val, llvm::raw_ostream&) const;
-    void printBlockInfo(const llvm::BasicBlock & block, llvm::raw_ostream&) const;
-    void dumpBlockInfo(const llvm::BasicBlock & block) const;
-    void printArguments(llvm::raw_ostream&) const;
-    void dumpArguments() const;
-
-
-    llvm::LLVMContext & getContext() const;
-    llvm::Function & getScalarFunction() { return *mapping.scalarFn; }
-    llvm::Function & getVectorFunction() { return *mapping.vectorFn; }
+  llvm::LLVMContext &getContext() const;
+  llvm::Function &getScalarFunction() { return *mapping.scalarFn; }
+  llvm::Function &getVectorFunction() { return *mapping.vectorFn; }
 };
 
-
-}
+} // namespace rv
 
 #endif /* INCLUDE_RV_VECTORIZATIONINFO_H_ */
--- src/vectorizationInfo.cpp
+++ src/vectorizationInfo.cpp
@@ -1,4 +1,5 @@
-//===----------------------- vectorizationInfo.cpp --------------------------===//
+//===----------------------- vectorizationInfo.cpp
+//--------------------------===//
 //
 //                     The Region Vectorizer
 //
@@ -8,162 +9,153 @@
 
 #include "rv/vectorizationInfo.h"
 
-#include <llvm/IR/Instruction.h>
+#include <llvm/Analysis/LoopInfo.h>
 #include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/Function.h>
-#include <llvm/Analysis/LoopInfo.h>
+#include <llvm/IR/Instruction.h>
 
 #include "rv/region/Region.h"
 #include "rvConfig.h"
 
 using namespace llvm;
 
-namespace rv
-{
+namespace rv {
 
-bool
-VectorizationInfo::inRegion(const BasicBlock & block) const {
+bool VectorizationInfo::inRegion(const BasicBlock &block) const {
   return region.contains(&block);
 }
 
-bool
-VectorizationInfo::inRegion(const Instruction & inst) const {
+bool VectorizationInfo::inRegion(const Instruction &inst) const {
   return region.contains(inst.getParent());
 }
 
-void
-VectorizationInfo::remapPredicate(Value& dest, Value& old)
-{
-    for (auto it : predicates)
-    {
-        if (it.second == &old)
-        {
-            predicates[it.first] = &dest;
-        }
+void VectorizationInfo::remapPredicate(Value &dest, Value &old) {
+  for (auto it : predicates) {
+    if (it.second == &old) {
+      predicates[it.first] = &dest;
     }
+  }
 }
 
-void
-VectorizationInfo::dump(const Value * val) const {
-  print(val, errs());
-}
+void VectorizationInfo::dump(const Value *val) const { print(val, errs()); }
 
-void
-VectorizationInfo::print(const Value * val, llvm::raw_ostream & out) const {
-  if (!val) return;
+void VectorizationInfo::print(const Value *val, llvm::raw_ostream &out) const {
+  if (!val)
+    return;
 
-  auto * block = dyn_cast<const BasicBlock>(val);
+  auto *block = dyn_cast<const BasicBlock>(val);
   if (block && inRegion(*block)) {
     printBlockInfo(*block, out);
   }
 
   if (hasKnownShape(*val)) {
-  	out << *val << " : " << getVectorShape(*val).str() << "\n";
+    out << *val << " : " << getVectorShape(*val).str() << "\n";
   } else {
-  	out << *val << " : unknown shape\n";
+    out << *val << " : unknown shape\n";
   }
 }
 
-void
-VectorizationInfo::dumpBlockInfo(const BasicBlock & block) const {
+void VectorizationInfo::dumpBlockInfo(const BasicBlock &block) const {
   printBlockInfo(block, errs());
 }
 
-void
-VectorizationInfo::printBlockInfo(const BasicBlock & block, llvm::raw_ostream & out) const {
-  const Value * predicate = getPredicate(block);
+void VectorizationInfo::printBlockInfo(const BasicBlock &block,
+                                       llvm::raw_ostream &out) const {
+  const Value *predicate = getPredicate(block);
 
   out << "Block ";
   block.printAsOperand(out, false);
-  if (predicate) { out << ", predicate " << *predicate; }
-  if (isDivergentLoopExit(block)) { out << ", divLoopExit"; }
+  if (predicate) {
+    out << ", predicate " << *predicate;
+  }
+  if (isDivergentLoopExit(block)) {
+    out << ", divLoopExit";
+  }
   out << "\n";
 
-  for (const Instruction & inst : block) {
+  for (const Instruction &inst : block) {
     print(&inst, out);
   }
   out << "\n";
 }
 
-void VectorizationInfo::dumpArguments() const {
-  printArguments(errs());
-}
+void VectorizationInfo::dumpArguments() const { printArguments(errs()); }
 
-void VectorizationInfo::printArguments(llvm::raw_ostream & out) const {
-  const Function* F = mapping.scalarFn;
+void VectorizationInfo::printArguments(llvm::raw_ostream &out) const {
+  const Function *F = mapping.scalarFn;
 
   out << "\nArguments:\n";
 
-  for (auto& arg : F->args()) {
-    out << arg << " : " << (hasKnownShape(arg) ? getVectorShape(arg).str() : "missing") << "\n";
+  for (auto &arg : F->args()) {
+    out << arg << " : "
+        << (hasKnownShape(arg) ? getVectorShape(arg).str() : "missing") << "\n";
   }
 
   out << "\n";
 }
 
-void
-VectorizationInfo::dump() const {
-  print(errs());
+void VectorizationInfo::dump() const { print(errs()); }
+
+void VectorizationInfo::print(llvm::raw_ostream &out) const {
+  out << "VectorizationInfo ";
+
+  out << "for " << region.str() << "\n";
+
+  printArguments(out);
+
+  for (const BasicBlock &block : *mapping.scalarFn) {
+    if (!inRegion(block))
+      continue;
+    printBlockInfo(block, out);
+  }
+
+  out << "}\n";
 }
 
-void
-VectorizationInfo::print(llvm::raw_ostream & out) const
-{
-    out << "VectorizationInfo ";
-
-    out << "for " << region.str() << "\n";
-
-    printArguments(out);
-
-    for (const BasicBlock & block : *mapping.scalarFn) {
-      if (!inRegion(block)) continue;
-      printBlockInfo(block, out);
-    }
-
-    out << "}\n";
-}
-
-VectorizationInfo::VectorizationInfo(llvm::Function& parentFn, unsigned vectorWidth, Region& _region)
-: DL(parentFn.getParent()->getDataLayout()), region(_region), mapping(&parentFn, &parentFn, vectorWidth, CallPredicateMode::SafeWithoutPredicate)
-{
-    mapping.resultShape = VectorShape::uni();
-    for (auto& arg : parentFn.args()) {
-      RV_UNUSED(arg);
-      mapping.argShapes.push_back(VectorShape::uni());
-    }
+VectorizationInfo::VectorizationInfo(llvm::Function &parentFn,
+                                     unsigned vectorWidth, Region &_region)
+    : DL(parentFn.getParent()->getDataLayout()), region(_region),
+      mapping(&parentFn, &parentFn, vectorWidth,
+              CallPredicateMode::SafeWithoutPredicate) {
+  mapping.resultShape = VectorShape::uni();
+  for (auto &arg : parentFn.args()) {
+    RV_UNUSED(arg);
+    mapping.argShapes.push_back(VectorShape::uni());
+  }
 }
 
 // VectorizationInfo
-VectorizationInfo::VectorizationInfo(Region & _region, VectorMapping _mapping)
-: DL(_region.getFunction().getParent()->getDataLayout()), region(_region), mapping(_mapping)
-{
+VectorizationInfo::VectorizationInfo(Region &_region, VectorMapping _mapping)
+    : DL(_region.getFunction().getParent()->getDataLayout()), region(_region),
+      mapping(_mapping) {
   assert(mapping.argShapes.size() == mapping.scalarFn->arg_size());
   auto it = mapping.scalarFn->arg_begin();
-  for (auto argShape : mapping.argShapes)
-  {
-    auto & arg = *it;
+  for (auto argShape : mapping.argShapes) {
+    auto &arg = *it;
     setPinned(arg);
     setVectorShape(arg, argShape);
     ++it;
   }
 }
 
-bool
-VectorizationInfo::hasKnownShape(const llvm::Value& val) const {
+bool VectorizationInfo::hasKnownShape(const llvm::Value &val) const {
 
   // explicit shape annotation take precedence
-    if ((bool) shapes.count(&val)) return true;
+  if ((bool)shapes.count(&val))
+    return true;
 
   // in-region instruction must have an explicit shape
-    auto * inst = dyn_cast<Instruction>(&val);
-    if (inst && inRegion(*inst)) return false;
+  auto *inst = dyn_cast<Instruction>(&val);
+  if (inst && inRegion(*inst))
+    return false;
 
   // out-of-region values default to uniform
-    return true;
+  return true;
 }
 
-VectorShape
-VectorizationInfo::getObservedShape(const LoopInfo & LI, const BasicBlock & observerBlock, const llvm::Value & val) const {
+VectorShape VectorizationInfo::getObservedShape(const LoopInfo &LI,
+                                                const BasicBlock &observerBlock,
+                                                const llvm::Value &val) const {
   auto valShape = getVectorShape(val);
   uint alignment = valShape.getAlignmentGeneral();
 
@@ -174,149 +166,120 @@ VectorizationInfo::getObservedShape(const LoopInfo & LI, const BasicBlock & obse
   return valShape;
 }
 
-VectorShape
-VectorizationInfo::getVectorShape(const llvm::Value& val) const
-{
-// Undef short-cut
-  if (isa<UndefValue>(val)) return VectorShape::undef();
-   auto it = shapes.find(&val);
+VectorShape VectorizationInfo::getVectorShape(const llvm::Value &val) const {
+  // Undef short-cut
+  if (isa<UndefValue>(val))
+    return VectorShape::undef();
+  auto it = shapes.find(&val);
 
-// give precedence to user shapes
+  // give precedence to user shapes
   if (it != shapes.end()) {
     return it->second;
   }
 
-// return default shape for constants
-  auto * constVal = dyn_cast<Constant>(&val);
+  // return default shape for constants
+  auto *constVal = dyn_cast<Constant>(&val);
   if (constVal) {
     return VectorShape::fromConstant(constVal);
   }
 
-// out-of-region values default to uniform
-  auto * inst = dyn_cast<Instruction>(&val);
+  // out-of-region values default to uniform
+  auto *inst = dyn_cast<Instruction>(&val);
   if (!inst || (inst && !inRegion(*inst))) {
     return VectorShape::uni(); // TODO getAlignment(*inst));
   }
 
-// otw, the shape is undefined
+  // otw, the shape is undefined
   return VectorShape::undef();
 }
 
-const DataLayout &
-VectorizationInfo::getDataLayout() const { return DL; }
+const DataLayout &VectorizationInfo::getDataLayout() const { return DL; }
 
-void
-VectorizationInfo::dropVectorShape(const Value& val)
-{
-    auto it = shapes.find(&val);
-    if (it == shapes.end()) return;
-    shapes.erase(it);
+void VectorizationInfo::dropVectorShape(const Value &val) {
+  auto it = shapes.find(&val);
+  if (it == shapes.end())
+    return;
+  shapes.erase(it);
 }
 
-
-void
-VectorizationInfo::setVectorShape(const llvm::Value& val, VectorShape shape)
-{
-    shapes[&val] = shape;
+void VectorizationInfo::setVectorShape(const llvm::Value &val,
+                                       VectorShape shape) {
+  shapes[&val] = shape;
 }
-
 
 // predicate handling
-void
-VectorizationInfo::dropPredicate(const BasicBlock& block)
-{
-    auto it = predicates.find(&block);
-    if (it == predicates.end()) return;
-    predicates.erase(it);
+void VectorizationInfo::dropPredicate(const BasicBlock &block) {
+  auto it = predicates.find(&block);
+  if (it == predicates.end())
+    return;
+  predicates.erase(it);
 }
 
-llvm::Value*
-VectorizationInfo::getPredicate(const llvm::BasicBlock& block) const
-{
-    auto it = predicates.find(&block);
-    if (it == predicates.end())
-    {
-        return nullptr;
-    }
-    else
-    {
-        return it->second;
-    }
+llvm::Value *
+VectorizationInfo::getPredicate(const llvm::BasicBlock &block) const {
+  auto it = predicates.find(&block);
+  if (it == predicates.end()) {
+    return nullptr;
+  } else {
+    return it->second;
+  }
 }
 
-void
-VectorizationInfo::setPredicate(const llvm::BasicBlock& block, llvm::Value& predicate)
-{
-    predicates[&block] = &predicate;
+void VectorizationInfo::setPredicate(const llvm::BasicBlock &block,
+                                     llvm::Value &predicate) {
+  predicates[&block] = &predicate;
 }
-
 
 // loop divergence
-bool
-VectorizationInfo::addDivergentLoop(const Loop & loop) {
+bool VectorizationInfo::addDivergentLoop(const Loop &loop) {
   return mDivergentLoops.insert(&loop).second;
 }
 
-void
-VectorizationInfo::removeDivergentLoop(const Loop & loop) {
+void VectorizationInfo::removeDivergentLoop(const Loop &loop) {
   mDivergentLoops.erase(&loop);
 }
 
-bool
-VectorizationInfo::isDivergentLoop(const llvm::Loop& loop) const
-{
-    return mDivergentLoops.find(&loop) != mDivergentLoops.end();
+bool VectorizationInfo::isDivergentLoop(const llvm::Loop &loop) const {
+  return mDivergentLoops.find(&loop) != mDivergentLoops.end();
 }
 
-bool
-VectorizationInfo::isDivergentLoopTopLevel(const llvm::Loop& loop) const
-{
-    Loop* parent = loop.getParentLoop();
+bool VectorizationInfo::isDivergentLoopTopLevel(const llvm::Loop &loop) const {
+  Loop *parent = loop.getParentLoop();
 
-    return isDivergentLoop(loop) && (!parent || !isDivergentLoop(*parent));
+  return isDivergentLoop(loop) && (!parent || !isDivergentLoop(*parent));
 }
-
 
 // loop exit divergence
-bool
-VectorizationInfo::isDivergentLoopExit(const BasicBlock& BB) const {
-    return DivergentLoopExits.find(&BB) != DivergentLoopExits.end();
+bool VectorizationInfo::isDivergentLoopExit(const BasicBlock &BB) const {
+  return DivergentLoopExits.find(&BB) != DivergentLoopExits.end();
 }
 
-bool
-VectorizationInfo::addDivergentLoopExit(const BasicBlock& block) {
-    return DivergentLoopExits.insert(&block).second;
+bool VectorizationInfo::addDivergentLoopExit(const BasicBlock &block) {
+  return DivergentLoopExits.insert(&block).second;
 }
 
-void
-VectorizationInfo::removeDivergentLoopExit(const BasicBlock& block) {
-    DivergentLoopExits.erase(&block);
+void VectorizationInfo::removeDivergentLoopExit(const BasicBlock &block) {
+  DivergentLoopExits.erase(&block);
 }
-
 
 // pinned shape handling
-bool VectorizationInfo::isPinned(const Value& V) const {
+bool VectorizationInfo::isPinned(const Value &V) const {
   return pinned.count(&V) != 0;
 }
 
-void VectorizationInfo::setPinned(const Value& V) {
-  pinned.insert(&V);
+void VectorizationInfo::setPinned(const Value &V) { pinned.insert(&V); }
+
+LLVMContext &VectorizationInfo::getContext() const {
+  return mapping.scalarFn->getContext();
 }
 
-
-
-LLVMContext &
-VectorizationInfo::getContext() const { return mapping.scalarFn->getContext(); }
-
-BasicBlock&
-VectorizationInfo::getEntry() const {
+BasicBlock &VectorizationInfo::getEntry() const {
   return region.getRegionEntry();
 }
 
-bool
-VectorizationInfo::isTemporalDivergent(const LoopInfo & LI,
-                                       const BasicBlock &ObservingBlock,
-                                       const Value &Val) const {
+bool VectorizationInfo::isTemporalDivergent(const LoopInfo &LI,
+                                            const BasicBlock &ObservingBlock,
+                                            const Value &Val) const {
   const auto *Inst = dyn_cast<const Instruction>(&Val);
   if (!Inst)
     return false;
@@ -324,8 +287,7 @@ VectorizationInfo::isTemporalDivergent(const LoopInfo & LI,
   // proceeds to ObservingBlock
   for (const auto *Loop = LI.getLoopFor(Inst->getParent());
        Loop && inRegion(*Loop->getHeader()) && !Loop->contains(&ObservingBlock);
-       Loop = Loop->getParentLoop())
-  {
+       Loop = Loop->getParentLoop()) {
     if (isDivergentLoop(*Loop)) {
       return true;
     }
@@ -334,6 +296,4 @@ VectorizationInfo::isTemporalDivergent(const LoopInfo & LI,
   return false;
 }
 
-} /* namespace rv */
-
-
+} // namespace rv
