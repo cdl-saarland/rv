a9bd135a04588ac6f693909e76e8e7a22edec054
Simon Moll <moll@cs.uni-saarland.de>
2018-12-13 17:19:37 +0100

    clenshaw: fixed oob access

--- test/suite/test_052_clenshaw-loop.cpp
+++ test/suite/test_052_clenshaw-loop.cpp
@@ -7,7 +7,7 @@ foo(double *coeffs,int n,double *xs,double *ys,int m)
   for (int i=0;i<m;i++){
     double x = xs[i];
     double u0=0,u1=0,u2=0;
-    for (int k=n;k>=0;k--){
+    for (int k=n-1;k>=0;k--){
       u2 = u1;
       u1 = u0;
       u0 = 2*x*u1-u2+coeffs[k];
