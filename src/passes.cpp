75943572689a692c551e6ecd5b477d2d72f1a674
Richard Membarth <richard.membarth@uni-saarland.de>
2019-06-07 14:24:41 +0200

    Compile fix.

--- src/passes.cpp
+++ src/passes.cpp
@@ -2,7 +2,7 @@
 
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Utils.h"
-#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/AggressiveInstCombine/AggressiveInstCombine.h"
 #include "llvm/Transforms/IPO/AlwaysInliner.h"
 #include "rv/transform/loopExitCanonicalizer.h"
 
