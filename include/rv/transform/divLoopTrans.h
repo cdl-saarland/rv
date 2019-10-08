d54664d5a747dae6004b83cafed6266d063e3ab0
Simon Moll <simon.moll@emea.nec.com>
2019-10-08 19:19:42 +0200

    gDLT: license headers

--- include/rv/transform/divLoopTrans.h
+++ include/rv/transform/divLoopTrans.h
@@ -1,3 +1,12 @@
+//===- rv/transform/divLoopTrans.h - make divergent loops uniform --*- C++ -*-===//
+//
+// Part of the RV Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+
 #ifndef RV_TRANSFORM_DIVLOOPTRANS_H
 #define RV_TRANSFORM_DIVLOOPTRANS_H
 
