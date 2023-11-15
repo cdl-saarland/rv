52bf39d353b3207a5ba2148f154dd0a83aff6caa
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2023-11-15 13:05:05 +0100

    Fix CMake issues for library build.

--- rv-shared.cmake
+++
@@ -1,5 +0,0 @@
-function ( get_rv_llvm_dependency_libs OUT_VAR )
-  llvm_map_components_to_libnames ( RV_LLVM_TEMP_LIBS analysis irreader support core transformutils)
-    SET ( ${OUT_VAR} ${RV_LLVM_TEMP_LIBS} PARENT_SCOPE )
-endfunction( get_rv_llvm_dependency_libs )
-
