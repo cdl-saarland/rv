e4f87d96d8b346e30f35e099e3bd6eee50e9941d
Matthias Kurtenacker <matthias.kurtenacker@dfki.de>
2024-03-12 12:30:23 +0100

    LLVM 18 Support
    * Switch from SyncDependenceAnalysis to GenericSyncDependenceAnalysis
    * llvm/ADT/Triple.h deprecated, changed to llvm/TargetParser/Triple.h

--- src/PlatformInfo.cpp
+++ src/PlatformInfo.cpp
@@ -18,7 +18,7 @@
 #include <llvm/IR/Module.h>
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/Support/TypeSize.h>
-#include <llvm/ADT/Triple.h>
+#include <llvm/TargetParser/Triple.h>
 
 #include "rvConfig.h"
 
